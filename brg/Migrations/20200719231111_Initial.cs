b8c843e03e081695b8da2b08330c6d41587029b9
RafaelMenezess <rafa.rcm@hotmail.com>
2020-07-19 20:24:52 -0300

    Conexao Mysql

---
+++ brg/Migrations/20200719231111_Initial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace brg.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Colaborador",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Cargo = table.Column<string>(nullable: true),
+                    Departamento = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Colaborador", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Colaborador");
+        }
+    }
+}
--- brg/Startup.cs
+++ brg/Startup.cs
@@ -37,7 +37,8 @@ namespace brg
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<BRGContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("BRGContext")));
+                    options.UseMySql(Configuration.GetConnectionString("BRGContext"), builder => 
+                        builder.MigrationsAssembly("brg")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
