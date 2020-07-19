b77e2224ac4dc7758e0f6dc743efd91fbe6e5c2f
RafaelMenezess <rafa.rcm@hotmail.com>
2020-07-19 19:20:07 -0300

    MVC Colaborador

---
+++ brg/Controllers/ColaboradoresController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BRG.Models;
+
+namespace BRG.Controllers
+{
+    public class ColaboradoresController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Colaborador> list = new List<Colaborador>();
+            list.Add(new Colaborador { Id = 1, Cargo = "Soldador", Departamento = "Fabrica", Name = "Carlos" });
+            list.Add(new Colaborador { Id = 2, Cargo = "RH", Departamento = "Escritorio", Name = "Maria" });
+            return View(list);
+        }
+    }
+}
